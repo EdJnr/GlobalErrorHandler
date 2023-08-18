7cce2f2d1b14a8c812ce66e0240360952cee24f6
EdJnr <edwardabladei@gmail.com>
2023-08-18 16:49:40 +0000

    Add project files.

---
+++ Middlewares/GlobalExceptionHandler.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Text.Json;
+
+namespace GlobalErrorHandler.Middlewares
+{
+    public class GlobalExceptionHandler : IMiddleware
+    {
+        private readonly ILogger<GlobalExceptionHandler> _logger;
+
+        public GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+
+                ProblemDetails problem = new()
+                {
+                    Status = (int)HttpStatusCode.InternalServerError,
+                    Title = "An internal error has occured",
+                    Type= "Server Error",
+                    Detail= ex.Message
+                };
+
+                string errorResponse = JsonSerializer.Serialize(problem);
+
+                context.Response.ContentType = "application/json";
+
+                await context.Response.WriteAsync(errorResponse);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+using GlobalErrorHandler.Middlewares;
+
+namespace GlobalErrorHandler
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddTransient<GlobalExceptionHandler>();
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddLogging();
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+
+            app.UseMiddleware<GlobalExceptionHandler>();
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
